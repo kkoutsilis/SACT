e4633df6d040b038e782ea075f8c194f19351bb9
koutsilis1999 <kkoutsilis@gmail.com>
2022-03-19 15:58:21 +0000

    HCNN progress

--- src/main/java/org/example/HCNN.java
+++ src/main/java/org/example/HCNN.java
@@ -1,19 +1,69 @@
 package org.example;
 
-import java.util.Collections;
-import java.util.Set;
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.commons.math3.stat.inference.BinomialTest;
+
+import java.util.*;
 
 public class HCNN {
+    private Graph graph;
+    private int n;
+    private Map<String,Set<String>> knn;
+    private Map<String,Set<String>> rkkn;
 
-    public Set<String> fit(Set<Integer> indexes, Set<String> data){
+    public HCNN() {
+    }
+    public HCNN(Graph graph, int n, Map<String,Set<String>> knn , Map<String,Set<String>> rknn){
+        this.graph = graph;
+        this.n = n;
+        this.knn = knn;
+        this.rkkn = rknn;
+    }
+    public Set<String> fit(){
+        return Collections.emptySet();
+    }
+    // types are probably wrong
+    private Set<String> assignOutliers(Map<String,Set<String>> clusters, Set<Integer> indexes, Set<Integer> outliers ){
+        Map<String,Integer> label = new HashMap<>();
+
+        for (int i=1; i <= clusters.size(); i++){
+            for (String j : clusters.get(Integer.toString(i))){
+                label.put(j,i);
+            }
+        }
+        for (Integer o: outliers){
+            int t = o;
+            if (!this.knn.get(Integer.toString(o)).isEmpty()){
+                int minStractSim = Integer.MAX_VALUE;
+                for (String i : this.knn.get(Integer.toString(o))){
+                   int stractSim = z(o, Integer.parseInt(i));
+                   if (minStractSim > stractSim) {
+                       minStractSim = stractSim;
+                   }
+               }
+                t = minStractSim;
+            }
+            if (label.get(Integer.toString(t)) == 0){
+
+            }
+
+        }
+        return Collections.emptySet();
+    }
+     // this will have to return 2 Sets
+    private Set<String> initializeClustering(){
         return Collections.emptySet();
     }
 
-    private Set<String> assignOutliers(Set<String> clusters, Set<Integer> indexes){
-        return Collections.emptySet();
-    }
+    //probably wrong
+    private int z(int i, int j){
+        Set<String> intersection = new LinkedHashSet<>(this.knn.get(Integer.toString(i)));
+        intersection.retainAll(this.knn.get(Integer.toString(j)));
 
-    private Set<String> initializeClustering(Set<Integer> indexes, Set<String> nn, Set<String> rknn){
-        return Collections.emptySet();
+        Set<String> union = new LinkedHashSet<>(knn.get(i));
+        union.addAll(this.knn.get(Integer.toString(j)));
+
+        return intersection.size()/union.size();
+
     }
 }
