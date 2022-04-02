8c44acb90bd91a2f99043604e00e46fbfa6175d3
koutsilis1999 <kkoutsilis@gmail.com>
2022-04-02 18:00:59 +0100

    refactor graph types to int

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -14,23 +14,23 @@ public class Graph {
         this.vertices = vertices;
     }
 
-    public void addVertex(String label) {
+    public void addVertex(int label) {
         vertices.putIfAbsent(new Vertex(label), new LinkedHashSet<>());
     }
 
-    public void removeVertex(String label) {
+    public void removeVertex(int label) {
         Vertex v = new Vertex(label);
-        vertices.values().stream().forEach(e -> e.remove(v));
+        vertices.values().forEach(e -> e.remove(v));
         vertices.remove(v);
     }
 
-    public void addEdge(String label1, String label2) {
+    public void addEdge(int label1, int label2) {
         Vertex v1 = new Vertex(label1);
         Vertex v2 = new Vertex(label2);
         vertices.get(v1).add(v2);
     }
 
-    public void removeEdge(String label1, String label2) {
+    public void removeEdge(int label1, int label2) {
         Vertex v1 = new Vertex(label1);
         Vertex v2 = new Vertex(label2);
         Set<Vertex> eV1 = vertices.get(v1);
@@ -43,7 +43,7 @@ public class Graph {
         return this.vertices;
     }
 
-    public Set<Vertex> getEdges(String label) {
+    public Set<Vertex> getEdges(int label) {
         return vertices.get(new Vertex(label));
     }
 
