7053788fea967681299f302ebe888d94fb5cc04a
Konstantinos Koutsilis <spod@Konstantinoss-MacBook-Pro.local>
2022-07-09 14:37:06 +0100

    add dump to csv method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,25 +1,55 @@
 package org.example;
 
-import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
+import com.opencsv.CSVWriter;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         Graph graph = new Graph();
         graph.parseCSV("CSVs/test.csv");
-        Map<Vertex, Set<Vertex>> knnRes = knn(3, graph);
+        Map<Vertex, Set<Vertex>> knnRes = knn(5, graph);
         Map<Vertex, Set<Vertex>> rknnRes = rknn(knnRes);
         Map<Vertex, Set<Vertex>> mknnRes = mknn(knnRes, rknnRes);
 
-        HCNN algo = new HCNN(graph, 5, knnRes, rknnRes);
+        HCNN algo = new HCNN(graph, 10, knnRes, rknnRes);
         List<Set<Vertex>> res = algo.fit();
-        for (Set<Vertex> s : res) {
-            System.out.println("------------------------------CLUSTER------------------------------");
-            System.out.println(s);
-            System.out.println();
-        }
+        dumpToCSV("Out/res1.csv", res);
+    }
 
+    public static void dumpToCSV(String path, List<Set<Vertex>> data) {
+        File file = new File(path);
+        try {
+            FileWriter outputfile = new FileWriter(file);
+
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            String[] header = { "CLUSTER_ID", "SCHEMA", "NAME", "TYPE" };
+            writer.writeNext(header);
+
+            int clusterID = 0;
+            for (Set<Vertex> vertexSet : data) {
+                for (Vertex vertex : vertexSet) {
+                    String[] output = { Integer.toString(clusterID), vertex.getSchema(),
+                            Integer.toString(vertex.getLabel()), vertex.getType() };
+                    writer.writeNext(output);
+                }
+                clusterID++;
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // return the k nearest neighbours for each vertex of a graph
