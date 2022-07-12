4e32727b302aacb03638efaab2298ea3d370c7e8
koutsilis1999 <kkoutsilis@gmail.com>
2022-07-12 19:20:39 +0100

    NearestNeighbour util class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,70 +1,20 @@
 package org.example;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Graph graph = CsvHandler.parseCSV("CSVs/test.csv");
 
-        Map<Vertex, Set<Vertex>> knnRes = knn(5, graph);
-        Map<Vertex, Set<Vertex>> rknnRes = rknn(knnRes);
-        Map<Vertex, Set<Vertex>> mknnRes = mknn(knnRes, rknnRes);
+        Map<Vertex, Set<Vertex>> knnRes = NearestNeighbour.knn(5, graph);
+        Map<Vertex, Set<Vertex>> rKnnRes = NearestNeighbour.rknn(knnRes);
+        Map<Vertex, Set<Vertex>> mKnnRes = NearestNeighbour.mknn(knnRes, rKnnRes);
 
-        HCNN algo = new HCNN(graph, 10, knnRes, rknnRes);
+        HCNN algo = new HCNN(graph, 10, knnRes, rKnnRes);
         List<Set<Vertex>> res = algo.fit();
 
         CsvHandler.dumpToCSV("Out/res3.csv", res);
-    }
-
-
-    // return the k nearest neighbours for each vertex of a graph
-    public static Map<Vertex, Set<Vertex>> knn(int k, Graph graph) {
-        Map<Vertex, Set<Vertex>> nearestNeighbours = new HashMap<>();
-        Set<Vertex> indexes = new LinkedHashSet<>(graph.getVertices().keySet());
-        for (Vertex index : indexes) {
-            Set<Vertex> visited = new LinkedHashSet<>();
-            Set<Vertex> neighbours = new LinkedHashSet<>();
-            Queue<Vertex> queue = new LinkedList<>();
-            queue.add(index);
-            visited.add(index);
-            while (!queue.isEmpty() && neighbours.size() < k) {
-                Vertex vertex = queue.poll();
-                for (Vertex v : graph.getEdges(vertex.getLabel())) {
-                    if (!visited.contains(v) && neighbours.size() < k) {
-                        visited.add(v);
-                        neighbours.add(v);
-                        queue.add(v);
-                    }
-                }
-            }
-            nearestNeighbours.put(index, neighbours);
-        }
-        return nearestNeighbours;
-    }
-
-    // reverse nearest neighbours of knn
-    public static Map<Vertex, Set<Vertex>> rknn(Map<Vertex, Set<Vertex>> knn) {
-        Map<Vertex, Set<Vertex>> reverseNearestNeighbours = new HashMap<>();
-        Set<Vertex> D = knn.keySet();
-        for (Vertex i : D) {
-            reverseNearestNeighbours.put(i, new LinkedHashSet<>());
-            for (Vertex j : D) {
-                if (knn.get(j).contains(i)) {
-                    reverseNearestNeighbours.get(i).add(j);
-                }
-            }
-        }
-        return reverseNearestNeighbours;
-    }
-
-    public static Map<Vertex, Set<Vertex>> mknn(Map<Vertex, Set<Vertex>> knn, Map<Vertex, Set<Vertex>> rknn) {
-        Map<Vertex, Set<Vertex>> mutualNearestNeighbours = new HashMap<>();
-        Set<Vertex> D = knn.keySet();
-        for (Vertex i : D) {
-            Set<Vertex> intersection = new LinkedHashSet<>(knn.get(i));
-            intersection.retainAll(rknn.get(i));
-            mutualNearestNeighbours.put(i, intersection);
-        }
-        return mutualNearestNeighbours;
     }
 }
---
+++ src/main/java/org/example/NearestNeighbour.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.util.*;
+
+public class NearestNeighbour {
+    private NearestNeighbour() {
+        throw new IllegalStateException("Utility class");
+
+    }
+
+    // return the k nearest neighbours for each vertex of a graph
+    public static Map<Vertex, Set<Vertex>> knn(int k, Graph graph) {
+        Map<Vertex, Set<Vertex>> nearestNeighbours = new HashMap<>();
+        Set<Vertex> indexes = new LinkedHashSet<>(graph.getVertices().keySet());
+        for (Vertex index : indexes) {
+            Set<Vertex> visited = new LinkedHashSet<>();
+            Set<Vertex> neighbours = new LinkedHashSet<>();
+            Queue<Vertex> queue = new LinkedList<>();
+            queue.add(index);
+            visited.add(index);
+            while (!queue.isEmpty() && neighbours.size() < k) {
+                Vertex vertex = queue.poll();
+                for (Vertex v : graph.getEdges(vertex.getLabel())) {
+                    if (!visited.contains(v) && neighbours.size() < k) {
+                        visited.add(v);
+                        neighbours.add(v);
+                        queue.add(v);
+                    }
+                }
+            }
+            nearestNeighbours.put(index, neighbours);
+        }
+        return nearestNeighbours;
+    }
+
+    // reverse nearest neighbours of knn
+    public static Map<Vertex, Set<Vertex>> rknn(Map<Vertex, Set<Vertex>> knn) {
+        Map<Vertex, Set<Vertex>> reverseNearestNeighbours = new HashMap<>();
+        Set<Vertex> D = knn.keySet();
+        for (Vertex i : D) {
+            reverseNearestNeighbours.put(i, new LinkedHashSet<>());
+            for (Vertex j : D) {
+                if (knn.get(j).contains(i)) {
+                    reverseNearestNeighbours.get(i).add(j);
+                }
+            }
+        }
+        return reverseNearestNeighbours;
+    }
+
+    public static Map<Vertex, Set<Vertex>> mknn(Map<Vertex, Set<Vertex>> knn, Map<Vertex, Set<Vertex>> rknn) {
+        Map<Vertex, Set<Vertex>> mutualNearestNeighbours = new HashMap<>();
+        Set<Vertex> D = knn.keySet();
+        for (Vertex i : D) {
+            Set<Vertex> intersection = new LinkedHashSet<>(knn.get(i));
+            intersection.retainAll(rknn.get(i));
+            mutualNearestNeighbours.put(i, intersection);
+        }
+        return mutualNearestNeighbours;
+    }
+}
